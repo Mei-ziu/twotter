8861bc79bdb61b7ca66061db6e0803e955bd9798
Mei-ziu <dylan02nguyen@gmail.com>
2022-08-03 18:07:42 -0700

    Adding Server Settings

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '@/store'
+import { users } from '@/assets/users'
 import HomeView from '../views/HomeView.vue'
 import UserProfile from '../views/UserProfile'
+import AdminView from '../views/AdminView.vue'
 
 const routes = [
   {
@@ -13,11 +16,33 @@ const routes = [
     name: 'UserProfile',
     component: UserProfile,
   },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: AdminView,
+    meta: {
+      requiresAdmin: true,
+    },
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach(async (to, from, next) => {
+  const user = store.state.User.user
+
+  if (!user) {
+    await store.dispatch('User/setUser', users[0])
+  }
+
+  const isAdmin = true
+  const requiresAdmin = to.matched.some((record) => record.meta.requiresAdmin)
+
+  if (requiresAdmin && !isAdmin) next({ name: 'Home' })
+  else next()
 })
 
 export default router
